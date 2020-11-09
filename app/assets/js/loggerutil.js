1785436e28860949d04443ee7ca1e99fdbdac595
Matwix <Matwix@users.noreply.github.com>
2020-11-09 13:20:30 +1100

    Added Launcher/Game File Logging

--- app/assets/js/loggerutil.js
+++ app/assets/js/loggerutil.js
@@ -1,3 +1,10 @@
+const fs = require('fs');
+const path = require('path')
+
+// Forked processes do not have access to electron, so we have this workaround.
+const launcherDir = process.env.CONFIG_DIRECT_PATH || require('electron').remote.app.getPath('userData')
+fs.writeFile(path.join(launcherDir, 'client.log'), '', function(){console.log('Cleared Client Logs!')})
+
 class LoggerUtil {
 
     constructor(prefix, style){
@@ -7,22 +14,27 @@ class LoggerUtil {
 
     log(){
         console.log.apply(null, [this.prefix, this.style, ...arguments])
+        fs.appendFileSync(path.join(launcherDir, 'client.log'), this.prefix.substring(2)+" "+[...arguments]+"\r\n");
     }
 
     info(){
         console.info.apply(null, [this.prefix, this.style, ...arguments])
+        fs.appendFileSync(path.join(launcherDir, 'client.log'), this.prefix.substring(2)+" "+[...arguments]+"\r\n");
     }
 
     warn(){
         console.warn.apply(null, [this.prefix, this.style, ...arguments])
+        fs.appendFileSync(path.join(launcherDir, 'client.log'), this.prefix.substring(2)+" "+[...arguments]+"\r\n");
     }
 
     debug(){
         console.debug.apply(null, [this.prefix, this.style, ...arguments])
+        fs.appendFileSync(path.join(launcherDir, 'client.log'), this.prefix.substring(2)+" "+[...arguments]+"\r\n");
     }
 
     error(){
         console.error.apply(null, [this.prefix, this.style, ...arguments])
+        fs.appendFileSync(path.join(launcherDir, 'client.log'), this.prefix.substring(2)+" "+[...arguments]+"\r\n");
     }
 
 }
